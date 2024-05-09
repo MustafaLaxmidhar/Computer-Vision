3ba2693f07e5f847e4cae41ac42f5c8344b61a1b
Chante <chantelee.dv@gmail.com>
2024-05-09 15:20:04 +1200

    added remove light and add dark functions

--- ColourProcessing.py
+++ ColourProcessing.py
@@ -32,3 +32,39 @@ def increase_contrast(image):
 
     return contrast_stretched
 
+import cv2
+
+# Load the image in grayscale
+def add_dark(image):
+
+    # Iterate through each pixel
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            # Check if pixel intensity is greater than 5
+            if image[i, j] < 70:
+                # Set pixel intensity to a lower value
+                image[i, j] = 0
+
+    # Display the modified image
+    cv2.imshow("dark Image", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    return image
+
+def remove_light(image):
+
+    # Iterate through each pixel
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            # Check if pixel intensity is greater than 5
+            if image[i, j] > 50:
+                # Set pixel intensity to a lower value
+                image[i, j] = 254
+
+    # Display the modified image
+    cv2.imshow("light Image", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    return image
--- Filter1.py
+++ Filter1.py
@@ -10,8 +10,8 @@ from matplotlib import pyplot as plt
 #pathfile for image 
 # Define the path to the image and the subfolder
 parentfolder = "WeldGapImages"
-subfolder = "Set 1"
-image_name = "image0001.jpg"  # or whatever the image file name is
+subfolder = "Set 3"
+image_name = "image0300.jpg"  # or whatever the image file name is
 src = os.path.join(parentfolder, subfolder, image_name)
 
 # opens image in grayscale
@@ -32,8 +32,10 @@ cv2.imshow("contrasted image", contrasted_image)
 
 # Apply Gaussian blur to reduce noise
 blurred_image = cv2.GaussianBlur(contrasted_image, (5, 7), 0)
+add_dark_image = ColourProcessing.add_dark(blurred_image)
+removed_light_image = ColourProcessing.remove_light(add_dark_image)
 
-edges = EdgeDetection.find_edges(blurred_image)
+edges = EdgeDetection.find_edges(removed_light_image)
 cv2.imshow("edged image", edges)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
